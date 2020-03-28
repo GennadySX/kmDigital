fa63fb199fd01c4549ee1fe83f8d661b0ff088c6
GennadySX <htcucell@gmail.com>
2020-03-28 10:33:01 +0300

    start

--- gulpfile.js
+++ gulpfile.js
@@ -16,34 +16,40 @@ var gulp 			= require('gulp'),
 /***SASS**/
 
 gulp.task('sass', function() {								/*****/
-	return gulp.src('src/sass/**/*.+(sass|scss)')  
-	.pipe(sass().on('error', sass.logError))
-	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7']),  { cascade: true })
-	.pipe(gulp.dest('src/css'))
-	.pipe(browserSync.reload({stream: true}))
+	return gulp.src('src/sass/**/*.sass')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7']),  { cascade: true })
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true}))
 });
+
+gulp.task('scss', function() {								/*****/
+	return gulp.src('src/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7']),  { cascade: true })
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+
 
 /***JS**/
 
-gulp.task('scripts', function() {
-    return  gulp.src([
-    					'src/libs/jquery/dist/jquery.min.js',
-    					'src/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
-  					 ])
-				.pipe(concat('libs.min.js'))
-				.pipe(uglify())
-				.pipe(gulp.dest('src/js'));
+gulp.task('scripts', function(){
+	return gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('js'))
+		.pipe(browserSync.reload({stream:true}));
 });
-
 
 /***CSS_LIBS***/
 
 gulp.task('css-libs', function() {
-    return gulp.src('src/sass/**/*.+(scss | scss)')
-	.pipe(sass())
-    .pipe(cssnano())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('src/css'));
+	return gulp.src('src/sass/**/*.+(sass | scss)')
+		.pipe(sass())
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('src/css'));
 });
 
 
@@ -74,9 +80,10 @@ gulp.task('img', function() {
 });
 
 gulp.task('watch', function() {
+	gulp.watch('./src/sass/**/*.sass', gulp.series('sass'));
 	gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-	gulp.watch('src/*.html',gulp.series('html'));
-	gulp.watch(['src/js/**/*.js','src/libs/**/*.js'], gulp.parallel('scripts'));    
+	gulp.watch('./src/*.html',gulp.series('html'));
+	gulp.watch('./src/js/*.js', gulp.parallel('scripts'));
 });
 
 gulp.task('clean',async function() {
@@ -87,17 +94,17 @@ gulp.task('bcss', function() {
    return gulp.src([
 		'src/css/*.css',
 		'src/css/*.min.css'])
-	  	.pipe(gulp.dest('dist/css'))
+	  	.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('bfonts', function() {
    return gulp.src('src/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'))
+		.pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('bjs', function() {
    return gulp.src('src/js/**/*.js')
-	  	.pipe(gulp.dest('dist/js'))
+	  	.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('bhtml', function() {
@@ -109,7 +116,7 @@ gulp.task('bhtml', function() {
 
 gulp.task('build', gulp.parallel('clean', 'img', 'sass', 'scripts','bcss','bjs','bfonts','bhtml'));
 
-gulp.task('start', gulp.parallel('sass','css-libs' ,'browser-sync','scripts', 'watch'));
+gulp.task('start', gulp.parallel('sass','scss' ,'browser-sync','scripts', 'watch'));
 
 
 gulp.task('clear', function() {
---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    console.log('data test 1000');
+
+    $(".block-1").mousemove(function (e) {
+        var y = e.pageY,
+            x = e.pageX,
+            top = 50,
+            left = 50;
+        $('.background').css({
+            'left': left - (x / 150) + "%",
+            'top': top - (y / 150) + "%",
+        })
+
+    });
+
+
+    if (window.innerWidth > 430) {
+        $('#pagepiling').pagepiling({
+            menu: '#menu',
+            anchors: ['page1', 'page2', 'page3', 'page4'],
+            navigation: false,
+            afterRender: function () {
+                $('#pp-nav').addClass('custom');
+            },
+            afterLoad: function (anchorLink, index) {
+                if (index > 1) {
+                    $('.d1').addClass('active');
+                    $('.d0').removeClass('active');
+                } else {
+                    $('.d0').addClass('active');
+                    $('.d1').removeClass('active');
+                }
+            }
+        });
+    } else $('body').css('overflow', 'auto')
+
+
+});
